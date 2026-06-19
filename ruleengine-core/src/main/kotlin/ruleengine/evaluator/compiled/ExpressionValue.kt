30fd6048e313ba93f5b97daa9a697aa9191a2b11
Jörg Flade <joerg.flade@finapi.io>
2026-06-20 00:17:15 +0200

    More AST

---
+++ ruleengine-core/src/main/kotlin/ruleengine/evaluator/compiled/ExpressionValue.kt
@@ -0,0 +1,23 @@
+package ruleengine.evaluator.compiled
+
+import java.math.BigDecimal
+
+sealed interface ExpressionValue
+
+data class NumberExpressionValue(
+    val value: BigDecimal
+) : ExpressionValue
+
+data class TextExpressionValue(
+    val value: String
+) : ExpressionValue
+
+data class BooleanExpressionValue(
+    val value: Boolean
+) : ExpressionValue
+
+data class ArrayExpressionValue(
+    val values: List<ExpressionValue>
+) : ExpressionValue
+
+data object MissingExpressionValue : ExpressionValue
